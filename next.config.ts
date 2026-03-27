e37c9a86aebc526fa79dd81d1583fa59f75d952e
Murilo <murilogonzagaramiro@gmail.com>
2026-03-27 20:18:07 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'media.discordapp.net',
+      },
+      {
+        protocol: 'https',
         hostname: 'i.scdn.co',
       },
     ],
